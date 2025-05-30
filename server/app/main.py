278c37a6a24fb4442dd49260e6f2c1e0475e392d
wesleyykim3 <wesleyykim@gmail.com>
2025-05-29 22:41:48 -0700

    test

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from app.routes import hello, id
 load_dotenv()
+from app.routes import hello, id
 import uvicorn
 
 
