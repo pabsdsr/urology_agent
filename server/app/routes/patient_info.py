f5ef42dc7e0a25389606a0c46fafdfe62422aa05
wesleyykim3 <wesleyykim@gmail.com>
2025-06-04 22:37:25 -0700

    updated patient_info route to take in id

--- server/app/routes/patient_info.py
+++ server/app/routes/patient_info.py
@@ -10,10 +10,42 @@ router = APIRouter(
     tags = ["patient_info"]
 )
 
-@router.get("")
-async def get_patient_info(
-    id = "296015"
-):
+# @router.get("")
+# async def get_patient_info(id = "296015"):
+#     token = await token_service.get_token()
+#     headers={
+#                 "x-api-key": os.environ.get("modmed_api_key"),
+#                 "Authorization": f"Bearer {token}"
+#             }
+#     urls = [
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/Patient/{id}", # patient
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/Encounter?patient={id}", # encounters
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/DocumentReference?patient={id}", # documents
+
+#         # Clinical data
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/MedicationStatement?patient={id}", # medication statement
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/AllergyIntolerance?patient={id}", # allergies
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/Condition?patient={id}", # conditions
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/FamilyMemberHistory?patient={id}", # family history
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/ServiceRequest?patient={id}", # service requests
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/DiagnosticReport?patient={id}", # diagnostic report
+        
+#         f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/Task?code=PMRECALL&patient={id}", # tasks
+#         # f"https://stage.ema-api.com/ema-dev/firm/uropmsandbox460/ema/fhir/v2/Practitioner", # practitioner
+
+#         # probably don't need practioners but I need to ask my dad
+#     ]
+
+#     async with httpx.AsyncClient() as client:
+#         tasks = [client.get(url, headers=headers) for url in urls]
+#         responses = await asyncio.gather(*tasks)
+
+#         results = [response.json() for response in responses]
+
+#     return results
+
+@router.get("/{id}")
+async def get_patient_info(id):
     token = await token_service.get_token()
     headers={
                 "x-api-key": os.environ.get("modmed_api_key"),
